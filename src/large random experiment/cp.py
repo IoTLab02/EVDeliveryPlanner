72227eb61c6ea940f4576def62625e6d080f687e
IoTLab02 <liot46373@gmail.com>
2025-06-01 15:54:46 -0500

    Delete src directory

--- src/large random experiment/cp.py
+++
@@ -1,7 +0,0 @@
-class CP:
-    def __init__(self, latitude, longitude, local_id, global_id):
-        self.latitude = latitude
-        self.longitude = longitude
-        self.local_id = local_id
-        self.global_id = global_id
-        self.type = "cp"
